1e44532f857350d03c804957e1ffdaf9de3f0b5a
Raven <user@users-MacBook-Air.local>
2022-05-26 14:08:40 -0400

    seventh

--- creatapp.js
+++ creatapp.js
@@ -1,1 +1,26 @@
-console.log('running')
+
+
+const express = require('express')
+
+const app = express()
+
+const port = 5000
+
+//set up view engine
+app.set('view engine', 'ejs')
+app.set('views', './Views')
+
+// root route 
+app.get('/', (req, res) => {
+    res.render("311info", {
+
+
+    })
+
+})
+
+
+
+app.listen(port,() => {
+    console.log(`run forest! ${port}`)
+}) 
